1ac96fe3815c20df7aa9b070fe9d4fae1a24775e
juancturcios <juancturcios@hotmail.com>
2026-09-11 18:10:46 +0200

    Create seeder and test for get user Ref #6

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -1,6 +1,7 @@
 package es.upm.miw.devops.rest;
 
 import es.upm.miw.devops.User;
+import es.upm.miw.devops.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,14 @@ public class UserResource {
 
     public static final String USERS = "/user";
 
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/{id}")
     public User read(@PathVariable String id) {
-        return new User(id, "name-" + id, "familyName-" + id);
+        return this.userService.read(id);
     }
 }
